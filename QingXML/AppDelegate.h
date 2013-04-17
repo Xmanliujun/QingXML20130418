07ecbaeb46cb1a89282b005612ade9b117ebc15f
Xmanliujun <644240319@qq.com>
2013-04-17 15:44:25 +0800

    Initial Commit

---
+++ QingXML/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QingXML
+//
+//  Created by XmL on 13-4-17.
+//  Copyright (c) 2013年 XmL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
